5a3f864db3c00ca5e99a91ac3f3d34cf037766ce
amwtke <amwtke@gmail.com>
2015-09-06 19:25:30 +0800

    往往

--- LogHelper/LogHelper.cs
+++ LogHelper/LogHelper.cs
@@ -32,27 +32,27 @@ namespace LogHelper
 
         public static void WriteLogInfo(Type t, object message)
         {
-            Console.WriteLine("log info rprocess:" + System.Threading.Thread.CurrentThread.ManagedThreadId);
+            //Console.WriteLine("log info rprocess:" + System.Threading.Thread.CurrentThread.ManagedThreadId);
             log4net.ILog log = log4net.LogManager.GetLogger(t);
             log.Info(message);
         }
 
         public static void LogInfoAsync(Type t, object message)
         {
-            Console.WriteLine("into log infor:" + System.Threading.Thread.CurrentThread.ManagedThreadId);
+            //Console.WriteLine("into log infor:" + System.Threading.Thread.CurrentThread.ManagedThreadId);
             LogInfoHandler handler = new LogInfoHandler(WriteLogInfo);
             handler.BeginInvoke(t, message, LogInfoCallBack, handler);
         }
         public static void LogErrorAsync(Type t, Exception ex)
         {
-            Console.WriteLine("into error" + System.Threading.Thread.CurrentThread.ManagedThreadId);
+            //Console.WriteLine("into error" + System.Threading.Thread.CurrentThread.ManagedThreadId);
             LogErrorHandler handler = new LogErrorHandler(WriteError);
             handler.BeginInvoke(t, ex, LogInfoCallBack, handler);
         }
 
         public static void LogInfoCallBack(IAsyncResult ar)
         {
-            Console.WriteLine("log info call back:" + System.Threading.Thread.CurrentThread.ManagedThreadId);
+            //Console.WriteLine("log info call back:" + System.Threading.Thread.CurrentThread.ManagedThreadId);
             if (ar == null)
                 throw new Exception("WebRequestCallBacks3 fails,because ar is null!");
 
@@ -61,7 +61,7 @@ namespace LogHelper
         }
         public static void LogErrCallBack(IAsyncResult ar)
         {
-            Console.WriteLine("log err call back:" + System.Threading.Thread.CurrentThread.ManagedThreadId);
+            //Console.WriteLine("log err call back:" + System.Threading.Thread.CurrentThread.ManagedThreadId);
             if (ar == null)
                 throw new Exception("WebRequestCallBacks3 fails,because ar is null!");
 
---
+++ log4net.ElasticSearch/Common/HTTP/CallBacks/CallBacks.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+
+
+namespace log4net.ElasticSearch
+{
+    public static class WebRequestsCallBacks
+    {
+        public static void WebRequestCallBacks(IAsyncResult ar)
+        {
+            Console.WriteLine("call backs:" + System.Threading.Thread.CurrentThread.ManagedThreadId);
+            System.Net.WebRequest wr = ar.AsyncState as System.Net.WebRequest;
+            using (var httpResp = (HttpWebResponse)wr.GetResponse())
+            {
+                CheckResponse(httpResp);
+                using (var sr = new StreamReader(httpResp.GetResponseStream(), Encoding.UTF8))
+                {
+                    string s = sr.ReadToEnd();
+                    new MyProcess().Process(s);
+                }
+            }
+            
+        }
+
+        public static void WebRequestCallBacks2(IAsyncResult ar)
+        {
+            Console.WriteLine("call back:" + System.Threading.Thread.CurrentThread.ManagedThreadId);
+            if (ar == null)
+                throw new Exception("WebRequestCallBacks2 fails,because ar is null!");
+
+            SendHandler2 handler = ar.AsyncState as SendHandler2;
+            WebRequest wr = handler.EndInvoke(ar);
+            using (var httpResp = (HttpWebResponse)wr.GetResponse())
+            {
+                CheckResponse(httpResp);
+                using (var sr = new StreamReader(httpResp.GetResponseStream(), Encoding.UTF8))
+                {
+                    string s = sr.ReadToEnd();
+                    new MyProcess().Process(s);
+                }
+            }
+        }
+
+        public static void WebRequestCallBacks3(IAsyncResult ar)
+        {
+            Console.WriteLine("call back:" + System.Threading.Thread.CurrentThread.ManagedThreadId);
+            if (ar == null)
+                throw new Exception("WebRequestCallBacks3 fails,because ar is null!");
+
+            SendHandler3 handler = ar.AsyncState as SendHandler3;
+            WebRequest wr = handler.EndInvoke(ar);
+            using (var httpResp = (HttpWebResponse)wr.GetResponse())
+            {
+                CheckResponse(httpResp);
+                using (var sr = new StreamReader(httpResp.GetResponseStream(), Encoding.UTF8))
+                {
+                    string s = sr.ReadToEnd();
+                    new MyProcess().Process(s);
+                }
+            }
+        }
+
+        private static void CheckResponse(HttpWebResponse httpResp)
+        {
+            if (httpResp.StatusCode == HttpStatusCode.BadRequest)
+            {
+                var buff = new byte[httpResp.ContentLength];
+                using (var response = httpResp.GetResponseStream())
+                {
+                    if (response != null)
+                    {
+                        response.Read(buff, 0, (int)httpResp.ContentLength);
+                    }
+                }
+
+                throw new InvalidOperationException(
+                    string.Format("Some error occurred while sending request to Elasticsearch.{0}{1}",
+                        Environment.NewLine, Encoding.UTF8.GetString(buff)));
+
+            }
+        }
+    }
+}
