605108aa7de98b4ddda7a301ef3e39f5e6d5859f
amwtke <amwtke@gmail.com>
2015-09-07 10:00:11 +0800

    bizobject

---
+++ log4net.ElasticSearch/Common/Attribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace log4net.ElasticSearch
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class BizAttribute : Attribute
+    {
+        public BizAttribute()
+        { }
+    }
+}
---
+++ log4net.ElasticSearch/Common/bizs/BizObject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace log4net.ElasticSearch
+{
+    [Biz]
+    public class BizObject
+    {
+
+    }
+}
